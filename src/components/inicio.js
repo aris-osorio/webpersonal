1144a1f0844ca3019cdc3d375bb11184ad29d79b
aris-osorio <aris.osorio@alumnos.udg.mx>
2021-02-04 17:54:41 -0600

    editando efecto paradax

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -4,7 +4,7 @@ export default function Inicio() {
 
     return (
         <section id="inicio">
-            <div className="d-flex align-items-center bg-black vh-100 mandril">
+            <div className="d-flex align-items-center bg-black vh-100">
                 <div className="text-white col-8 fondo aparece">
                     <h1 className="title julius text-center m-0 p-0">Aris Osorio</h1>
                     <h2 className="subtitle julius text-center">Desarrollador&nbsp;
