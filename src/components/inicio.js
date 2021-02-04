81834ed7e1083c4a3972e42070bd95545dc2dd72
aris-osorio <aris.osorio@alumnos.udg.mx>
2021-02-04 16:59:05 -0600

    editando mandril img

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -3,15 +3,13 @@ import Typed from 'react-typed';
 export default function Inicio() {
 
     return (
-        <section id = "inicio">
-            <div className="mandril">
-                <div className="d-flex align-items-center">
-                    <div className="text-white col-8 fondo aparece">
-                        <h1 className="title julius text-center m-0 p-0">Aris Osorio</h1>
-                        <h2 className="subtitle julius text-center">Desarrollador&nbsp;
-                            <Typed strings={['Web','React','Django','Fullstack']} typeSpeed={40} backSpeed={50} startDelay={2000} backDelay ={1000}/>
-                        </h2>
-                    </div>
+        <section id="inicio">
+            <div className="d-flex align-items-center mandril vh-100">
+                <div className="text-white col-8 fondo aparece">
+                    <h1 className="title julius text-center m-0 p-0">Aris Osorio</h1>
+                    <h2 className="subtitle julius text-center">Desarrollador&nbsp;
+                            <Typed strings={['Web', 'React', 'Django', 'Fullstack']} typeSpeed={40} backSpeed={50} startDelay={2000} backDelay={1000} />
+                    </h2>
                 </div>
             </div>
         </section>
