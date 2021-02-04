fb76acc51fabf9ebfcd5e9dc76e8ff32a9ca1d62
aris-osorio <aris.osorio@alumnos.udg.mx>
2021-02-04 16:49:27 -0600

    editando mandril img

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -5,7 +5,7 @@ export default function Inicio() {
     return (
         <section id = "inicio">
             <div className="mandril">
-                <div className="vh-100 d-flex align-items-center">
+                <div className="d-flex align-items-center">
                     <div className="text-white col-8 fondo aparece">
                         <h1 className="title julius text-center m-0 p-0">Aris Osorio</h1>
                         <h2 className="subtitle julius text-center">Desarrollador&nbsp;
