db847d8cc2c2a47609b1eb47551d0b85e58b1302
aris-osorio <aris.osorio@alumnos.udg.mx>
2021-02-05 00:12:37 -0600

    arreglando navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,38 +1,42 @@
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 import { Link } from "react-scroll";
-
+import Logo from '../media/logo.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle } from '@fortawesome/free-regular-svg-icons'
 export default function NavBar() {
     return (
-        <Navbar id="nav" collapseOnSelect expand="lg" variant="dark" className="fixed-top" transition="fade">
-            <div className="col-4">
-                <Navbar.Brand href="/" className="julius yellow">Aris</Navbar.Brand>
+        <nav class="navbar fixed-top">
+            <div class="col-2">
+                <span class="navbar-brand mb-0 h1">
+                    <img src={Logo} alt="logo" width="20" height="20" className="pb-1" />&nbsp;&nbsp;arisoso.com
+                </span>
             </div>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav id ="NavContainer" className="col-6 d-flex justify-content-between">
-                    <li className="nav-item">
+            <div className="col-8 d-flex justify-content-center">
+                <div className="d-flex w-75 justify-content-around">
+                    <div className="nav-item">
                         <Link activeClass="active" to="inicio" spy={true} smooth={true} duration={500}>
                             Inicio
                         </Link>
-                    </li>
-                    <li className="nav-item">
+                    </div>
+                    <div className="nav-item">
                         <Link activeClass="active" to="acerca" spy={true} smooth={true} duration={500}>
                             Perfil
                         </Link>
-                    </li>
-                    <li className="nav-item">
+                    </div>
+                    <div className="nav-item">
                         <Link activeClass="active" to="proyectos" spy={true} smooth={true} duration={500}>
                             Proyectos
                         </Link>
-                    </li>
-                    <li className="nav-item">
+                    </div>
+                    <div className="nav-item">
                         <Link activeClass="active" to="contactame" spy={true} smooth={true} duration={500}>
                             Contactame
                         </Link>
-                    </li>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+                    </div>
+                </div>
+            </div>
+            <div className="col-2 d-flex justify-content-end">     
+                <FontAwesomeIcon icon={faCircle} className="fa-2x yellow icon"  />
+            </div>
+        </nav>
     )
 }
