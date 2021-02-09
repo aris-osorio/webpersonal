b3338bc0c777ae0fcf84a8563ea8eaef6994a34e
aris-osorio <aris.osorio@alumnos.udg.mx>
2021-02-09 04:54:44 -0600

    modificando parpadeo

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ export default function App() {
     let topVent = html.scrollTop;
     for(let i = 0; i < elementoAparece.length; i++){
       let topelemAparece = elementoAparece[i].offsetTop;
-      if(topVent > topelemAparece - 400){
+      if(topVent > topelemAparece - 200){
         elementoAparece[i].style.opacity = 1;
         links_menu[i].classList.add('bg-links')
         for(let j = 0; j < elementoAparece.length; j++){
