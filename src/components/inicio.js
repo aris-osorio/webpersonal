99a782d05c4c61d56e25d80e1de6ebf1d781523e
aris-osorio <aris.osorio@alumnos.udg.mx>
2021-02-09 03:18:29 -0600

    calculando manualmente altura en imagenmandril

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -5,7 +5,7 @@ export default function Inicio() {
     return (
         <section id="inicio">
             <div className="d-flex align-items-center mandril">
-                <div className="vh-100 col-8 d-flex align-items-center justify-content-center">
+                <div className="vh-100 col-8 pl-0 pr-0 d-flex align-items-center justify-content-center">
                     <div className="text-white fondo aparece">
                         <h1 className="title julius text-center m-0 p-0">Aris Osorio</h1>
                         <h2 className="subtitle julius text-center">Desarrollador&nbsp;
